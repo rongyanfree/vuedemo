61b0d8a8b778e37e6b2ecc13fb70aa867538206a
HouZhaoHui <rongyanfree@163.com>
2018-08-01 16:43:26 +0800

    rankinglist

--- src/components/my/rankinglistpage.vue
+++ src/components/my/rankinglistpage.vue
@@ -7,8 +7,8 @@
 	                <p></p>
 	            </div>-->
 	            <h4 style="text-align: center;line-height:0.8rem;">积分排行</h4>
-	            <div class="chooseMonth iDate full " style="width: 27%;margin-left: 67%;">
-	                <input type="text" name="" placeholder="请选择月份">
+	            <div class="chooseMonth iDate full" style="width: 27%;margin-left: 67%;">
+	                <input type="text" v-model="chooseMonth" name="" placeholder="请选择月份">
 	                <button type="button" class="addOn" style="border:none;"></button>
 	            </div>
 	            <div class="RankingList" v-for="(item,index) in rankingList">
@@ -43,18 +43,33 @@
 		name:'rankinglistpage',
 		data(){
 			return{
+				chooseMonth:'',
 				rankingList:{}
 			}
 		},
 		activated(){
+			var that = this;
+			let calendar1 = new datePicker();
+	      	calendar1.init({
+	         	'trigger': '.chooseMonth', /*选择器，触发弹出插件*/
+	         	'type': 'ym',/*date 调出日期选择 datetime 调出日期时间选择 time 调出时间选择 ym 调出年月选择*/
+	         	'minDate':'1900-1-1',/*最小日期*/
+	         	'maxDate':new Date().getFullYear()+'-'+(new Date().getMonth()+1)+'-'+new Date().getDate(),/*最大日期*/
+	         	'onSubmit':function(){/*确认时触发事件*/
+	             	that.chooseMonth = calendar1.value;
+
+	             	console.log(that.chooseMonth)
+	         	},
+	         	'onClose':function(){/*取消时触发事件*/
+
+	         	}
+	      });
 					
 		},
 		created(){
 			let that = this;
 	  		that.$http.get('../../static/json/getRankingListData.json').then(function(res){
-	  			console.log(res.body);
-	  			res.body.forEach(function (item,index,arr) {
-	  				console.log(item,index,arr)
+	  			res.body.forEach(function (item,index,arr){
 	  				if(index == 0){
 	  					item.imgsrc = '../../../static/img/icon_goldMedal.png';
 	  					item.imgflag = true;
